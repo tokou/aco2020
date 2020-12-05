75b9b65e72d2c8c21c8d85669c1fab9c901a548a
Tarek Belkahia <tarek.belkahia@360learning.com>
2020-12-05 23:35:45 +0100

    Update forest size

--- src/commonMain/kotlin/Forest.kt
+++ src/commonMain/kotlin/Forest.kt
@@ -1,11 +1,11 @@
 
-data class Forest(val height: Int, val width: Int, val trees: Set<Pair<Int, Int>>) {
+data class Forest(val size: Pair<Int, Int>, val trees: Set<Pair<Int, Int>>) {
 
     fun downSlope(slope: Pair<Int, Int> = 3 to 1): List<Pair<Int, Int>> {
         var current = 0 to 0
         val encountered = mutableListOf<Pair<Int, Int>>()
-        while (current.second < height) {
-            if (trees.contains((current.first % width) to current.second)) {
+        while (current.second < size.second) {
+            if (trees.contains((current.first % size.first) to current.second)) {
                 encountered.add(current)
             }
             current = (current.first + slope.first) to (current.second + slope.second)
@@ -21,7 +21,7 @@ data class Forest(val height: Int, val width: Int, val trees: Set<Pair<Int, Int>
                     .filter { it.first == '#' }
                     .map { it.second to i }
             }.toSet()
-            return Forest(lines.size, lines.first().length, coords)
+            return Forest(lines.first().length to lines.size, coords)
         }
     }
 }
